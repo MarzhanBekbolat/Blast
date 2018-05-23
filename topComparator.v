ef71c2d268527538be5419298d84aec22b711e61
MarzhanBekbolat <marzhan.bekbolat@nu.edu.kz>
2018-05-23 22:04:15 +0600

    comparator modified

--- topComparator.v
+++ topComparator.v
@@ -49,47 +49,47 @@ assign  score = $signed(scoreCalc) <= 0 ? 0 : scoreCalc;
   if( inQuery[21:20] == inDB[21:20])
         scoreCalc = $signed(scoreCalc) + 5;
     else 
-        scoreCalc = scoreCalc - 4;
+        scoreCalc = $signed(scoreCalc) - 4;
   if( inQuery[19:18] == inDB[19:18])
-                scoreCalc = scoreCalc + 5;
+                scoreCalc = $signed(scoreCalc) + 5;
             else 
-                scoreCalc = scoreCalc - 4;
+                scoreCalc = $signed(scoreCalc) - 4;
    if( inQuery[17:16] == inDB[17:16])
-                        scoreCalc = scoreCalc + 5;
+                        scoreCalc = $signed(scoreCalc) + 5;
                     else 
-     scoreCalc = scoreCalc - 4;  
+     scoreCalc = $signed(scoreCalc) - 4;  
     if( inQuery[15:14] == inDB[15:14])
-                             scoreCalc = scoreCalc + 5;
+                             scoreCalc = $signed(scoreCalc) + 5;
                          else 
-          scoreCalc = scoreCalc - 4;    
+          scoreCalc = $signed(scoreCalc) - 4;    
     if( inQuery[13:12] == inDB[13:12])
-                                  scoreCalc = scoreCalc + 5;
+                                  scoreCalc = $signed(scoreCalc) + 5;
                               else 
-               scoreCalc = scoreCalc - 4;  
+               scoreCalc = $signed(scoreCalc) - 4;  
      if( inQuery[11:10] == inDB[11:10])
-                                       scoreCalc = scoreCalc + 5;
+                                       scoreCalc = $signed(scoreCalc) + 5;
                                    else 
-                    scoreCalc = scoreCalc - 4; 
+                    scoreCalc = $signed(scoreCalc) - 4; 
     if( inQuery[9:8] == inDB[9:8])
-                                            scoreCalc = scoreCalc + 5;
+                                            scoreCalc = $signed(scoreCalc) + 5;
                                         else 
-                         scoreCalc = scoreCalc - 4;    
+                         scoreCalc <= $signed(scoreCalc) - 4;    
       if( inQuery[7:6] == inDB[7:6])
-          scoreCalc = scoreCalc + 5;
+          scoreCalc = $signed(scoreCalc) + 5;
          else 
-         scoreCalc = scoreCalc - 4;               
+         scoreCalc = $signed(scoreCalc) - 4;               
      if( inQuery[5:4] == inDB[5:4])
-                  scoreCalc = scoreCalc + 5;
+                  scoreCalc = $signed(scoreCalc) + 5;
                  else 
-                 scoreCalc = scoreCalc - 4;
+                 scoreCalc = $signed(scoreCalc) - 4;
       if( inQuery[3:2] == inDB[3:2])
-                                  scoreCalc = scoreCalc + 5;
+                                  scoreCalc = $signed(scoreCalc) + 5;
                                  else 
-                                 scoreCalc = scoreCalc - 4;  
+                                 scoreCalc = $signed(scoreCalc) - 4;  
        if( inQuery[1:0] == inDB[1:0])
-                                                  scoreCalc = scoreCalc + 5;
+                                                  scoreCalc = $signed(scoreCalc) + 5;
                                                  else 
-                                                 scoreCalc = scoreCalc - 4;                
+                                                 scoreCalc = $signed(scoreCalc)- 4;                
    // score <= scoreCalc;
   end
  end   
