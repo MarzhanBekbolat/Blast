00641654f5d6eed533fc0d848595f4308b2cdaff
sabinakairatova <36705022+sabinakairatova@users.noreply.github.com>
2018-09-05 15:35:36 +0600

    Update Hit.v

--- docs/Hit.v
+++ docs/Hit.v
@@ -1,32 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.05.2018 11:29:54
-// Design Name: 
-// Module Name: topModule
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Hit(
 input clk,
 input rst,
-//only once
 input [511:0] query,
 input queryValid,
-//data from ddr
 input [511:0] dataBase,
 input dataBaseValid,
 input shift,
@@ -34,24 +12,24 @@ input load,
 input stop,
 input [31:0] locationStart,
 input [31:0] locationEnd,
-//output
 output [8:0] ShiftNo,
 output wire hit,
 output reg startExpand,
-output [8:0] locationQ
+output [8:0] locationQ, 
+output reg highBitEnd
     );
-  localparam IDLE = 1'b0,
-             HITLOW = 1'b1;  
+ localparam IDLE = 1'b0,
+            HITLOW = 1'b1;  
  reg state;
  wire [21:0] dBCompareWmer;
  wire [511:0] dbShiftRegOut;
-wire [245:0] ouput;
+ wire [245:0] ouput;
  reg [8:0] CurrentLocation; 
  reg [511:0] queryReg; 
- //for output of comparators
  reg [8:0] ShiftNoIn=0; 
  reg [8:0] k;
  reg [31:0] locationStartReg;
+ 
  
  assign hit = |ouput;
  
@@ -70,68 +48,64 @@ wire [245:0] ouput;
  
  
  assign locationQ = CurrentLocation;
- assign dBCompareWmer = dbShiftRegOut[21:0];  //changed
+ assign dBCompareWmer = dbShiftRegOut[21:0];  
  assign ShiftNo = ShiftNoIn; 
  
  
  always @(posedge clk) 
    begin
-  if (shift)// if(shift)
+  if (shift)
    begin
     ShiftNoIn <= ShiftNoIn+2;
   end 
-  else if (stop)
-  begin
-  if(locationEnd -locationStartReg >= 200 )
-  ShiftNoIn <= locationStartReg +200;
-  else
-    ShiftNoIn <= locationEnd;
-  end
  end
 
-// At each clk find max of comparatpr outputs
 always @(posedge clk)
     begin
      if(rst)
         begin
-        CurrentLocation <=0;
-        startExpand <=0;
-        k <= 0;
-        state <=IDLE;
+           CurrentLocation <=0;
+           startExpand <=0;
+           k <= 0;
+           state <=IDLE;
         end 
      else 
        begin
-       case(state)
-       IDLE: begin
-       if(dataBaseValid & hit)
-       begin
-         //for(k=0; k<246; k= k+1)
-         //begin
-         if(ouput[k]==1)
+         case(state)
+         IDLE: begin
+           if(hit & dataBaseValid)
+           begin
+             if(ouput[k]==1)
              begin
-             startExpand <=1;
-             CurrentLocation <= k*2;
-             //k=246;
-             state <=HITLOW;
+                startExpand <=1;
+                CurrentLocation <= k*2;
+                state <=HITLOW;
              end
-         //end
-           else if(k < 246)
-                  k <= k+1;
+             else if(k < 246) /////////////////////////////////////////////////////////////////////
+                     k <= k+1;
+             else if(k == 246)
+             begin
+                  highBitEnd <= 1; 
+                  k <= 0;
+             end     
+           end
          end
-        end
-        HITLOW: begin
-         if(stop)
-         begin
-         k<=0;
-         state <=IDLE;
+         HITLOW: begin
+           if(stop)
+           begin
+            /* if(k < 246)
+               k <= k+1;
+             else */
+             startExpand <=0;
+               k <= 0;   
+           state <=IDLE;
+           end
          end
-        end
-        endcase
-      end
-   end
+         endcase
+       end
+    end
 
  
- //generate 246 comparators
  genvar i;
  generate 
  for(i =0; i<491; i =i+2)
@@ -143,23 +117,19 @@ always @(posedge clk)
     .dbValid(dataBaseValid),
     .inQuery(queryReg[i+:22]),
     .inDB(dBCompareWmer),
-    .isMatch(ouput[i/2])
+    .isMatch(ouput[i/2]),
+    .highBitEnd(highBitEnd)
         );
   end
  endgenerate
  
 
- //shift by two bits, load if new data needed
  shift2Reg dbShiftReg(
  .clk(clk),
  .rst(rst),
  .load(load),
  .shift(shift),
- .ShiftNo(ShiftNoIn),
- .stop(stop),
- .hit(hit),
  .inData(dataBase),
- .dataValid(dataBaseValid),
  .outData(dbShiftRegOut)
  );
 
